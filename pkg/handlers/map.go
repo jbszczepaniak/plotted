776bcfe07c27b94bfb21b66ac8f0c87457b606a9
Jędrzej Szczepaniak <jbszczepaniak@gmail.com>
2019-08-27 22:09:02 +0200

    Add suboptimal concurrency boost

--- pkg/handlers/map.go
+++ pkg/handlers/map.go
@@ -6,9 +6,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/antihax/optional"
+
 	"github.com/jedruniu/plotted/pkg/storage"
 	swagger "github.com/jedruniu/plotted/swagger-generated"
 	gopoly "github.com/twpayne/go-polyline"
@@ -49,59 +51,99 @@ func (m *MapServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	before = before.AddDate(0, 0, 1)
 
 	var activities []swagger.SummaryActivity
+	var activitiesMutex sync.Mutex
 
-	for i := 1; ; i++ {
-		opts.After = optional.NewInt32(int32(after.Unix()))
-		opts.Before = optional.NewInt32(int32(before.Unix()))
-		opts.Page = optional.NewInt32(int32(i))
-		opts.PerPage = optional.NewInt32(200)
+	var summaryWg sync.WaitGroup
+	summaryPool := make(chan int, 20)
 
-		summary, resp, err := client.ActivitiesApi.GetLoggedInAthleteActivities(reqCtx, &opts)
-		if err != nil {
-			http.Error(w, err.Error(), resp.StatusCode)
-			return
+	shouldFetch := true
+	for i := 1; shouldFetch; i++ {
+		summaryWg.Add(1)
+		go func(i int) {
+			defer summaryWg.Done()
+			defer func() {
+				<-summaryPool
+			}()
+			summaryPool <- 1
+			fmt.Printf("Asking for page %d\n", i)
+
+			opts.After = optional.NewInt32(int32(after.Unix()))
+			opts.Before = optional.NewInt32(int32(before.Unix()))
+			opts.Page = optional.NewInt32(int32(i))
+			opts.PerPage = optional.NewInt32(200)
+			summary, _, err := client.ActivitiesApi.GetLoggedInAthleteActivities(reqCtx, &opts)
+			if err != nil {
+				fmt.Printf("Error for page %dretutned empty\n", i)
+				return
+			}
+			if len(summary) == 0 {
+				fmt.Printf("Page %dretutned empty\n", i)
+				shouldFetch = false
+				return
+
+			}
+			fmt.Printf("Page %d OK\n", i)
+
+			activitiesMutex.Lock()
+			activities = append(activities, summary...)
+			activitiesMutex.Unlock()
+		}(i)
+		if i%5 == 0 {
+			time.Sleep(100 * time.Millisecond)
 		}
-		if len(summary) == 0 {
-			break
-		}
-		activities = append(activities, summary...)
 	}
+	summaryWg.Wait()
 
 	var polylines [][][]float64
+	var polylinesMutex sync.Mutex
+	var wg sync.WaitGroup
+	pool := make(chan int, 20)
 
 	for _, activity := range activities {
-		var polyline []byte
+		wg.Add(1)
+		go func(activity swagger.SummaryActivity) {
+			defer wg.Done()
+			defer func() {
+				<-pool
+			}()
+			pool <- 1
 
-		cachedPolyline := fmt.Sprintf("%d.polyline", activity.Id)
-		exists, _ := m.Cache.Exists(reqCtx, cachedPolyline)
-		if exists {
-			polyline, _ = m.Cache.Get(reqCtx, cachedPolyline)
-		} else {
-			detailed, _, err := client.ActivitiesApi.GetActivityById(reqCtx, activity.Id, nil)
+			var polyline []byte
+
+			cachedPolyline := fmt.Sprintf("%d.polyline", activity.Id)
+			exists, _ := m.Cache.Exists(reqCtx, cachedPolyline)
+			if exists {
+				polyline, _ = m.Cache.Get(reqCtx, cachedPolyline)
+			} else {
+				detailed, _, err := client.ActivitiesApi.GetActivityById(reqCtx, activity.Id, nil)
+				if err != nil {
+					log.Printf("err for activity %d, err: %v", activity.Id, err)
+					return
+				}
+				if detailed.Map_.Polyline == "" {
+					return
+				}
+				polyline = []byte(detailed.Map_.Polyline)
+
+				err = m.Cache.Set(reqCtx, cachedPolyline, polyline)
+				if err != nil {
+					panic(err)
+				}
+			}
+
+			var polylineDecoded [][]float64
+
+			polylineDecoded, _, err := gopoly.DecodeCoords(polyline)
 			if err != nil {
-				log.Printf("err for activity %d, err: %v", activity.Id, err)
-				continue
+				log.Printf("could not decode polyline from file %d, err: %v", activity.Id, err)
+			} else {
+				polylinesMutex.Lock()
+				polylines = append(polylines, polylineDecoded)
+				polylinesMutex.Unlock()
 			}
-			if detailed.Map_.Polyline == "" {
-				continue
-			}
-			polyline = []byte(detailed.Map_.Polyline)
-			err = m.Cache.Set(reqCtx, cachedPolyline, polyline)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		var polylineDecoded [][]float64
-
-		polylineDecoded, _, err := gopoly.DecodeCoords(polyline)
-		if err != nil {
-			log.Printf("could not decode polyline from file %d, err: %v", activity.Id, err)
-		} else {
-			polylines = append(polylines, polylineDecoded)
-		}
-
+		}(activity)
 	}
+	wg.Wait()
 
 	tmpl, _ := template.New("").Parse(MapHTML)
 
